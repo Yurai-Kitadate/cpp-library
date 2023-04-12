22620751f1e8cabee8096d91dc9d576d102b1260
Yurai-Kitadate <yuurai18@gmail.com>
2023-04-13 04:03:32 +0900

    ファイル名変更

--- test/aoj/NTL_1_B.cpp
+++
@@ -1,9 +0,0 @@
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/1/NTL_1_B"
-#include "../../template/template.hpp"
-#include "../../math/modpow.hpp"
-
-int main()
-{
-    LL(n, m);
-    out(modpow(n, m, 1000000007));
-}
