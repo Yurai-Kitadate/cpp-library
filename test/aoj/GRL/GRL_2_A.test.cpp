58a3fd234033540a74bc2bc52afaef4888bb2e41
Yurai-Kitadate <yuurai18@gmail.com>
2023-04-14 02:54:28 +0900

    重複を削除

--- test/aoj/GRL/GRL_2_A.test.cpp
+++ test/aoj/GRL/GRL_2_A.test.cpp
@@ -1,7 +1,5 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/2/GRL_2_A"
 #include "../../../template/template.hpp"
-#include "../../../structure/union-find.hpp"
-#include "../../../graph/graph-template/graph-template.hpp"
 #include "../../../graph/kruskal.hpp"
 int main()
 {
