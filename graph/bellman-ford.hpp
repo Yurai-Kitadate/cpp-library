b9e7e34fc237f6990b344adf82917cbd99c02c2b
Yurai-Kitadate <yuurai18@gmail.com>
2023-04-13 15:06:10 +0900

    syuusei

--- graph/bellman-ford.hpp
+++ graph/bellman-ford.hpp
@@ -12,7 +12,9 @@ pair<bool, vector<T>> bellman_ford(vector<Edge<T>> g, ll v, ll st)
         }
         for (auto next : g)
         {
-            if (next.from < LINF && d[next.to] > d[next.from] + next.cost)
+            if (d[next.from] == LINF)
+                continue;
+            if (d[next.to] > d[next.from] + next.cost)
             {
                 d[next.to] = d[next.from] + next.cost;
                 update = true;
