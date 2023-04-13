9c08d2c02338ecbeb7bd93b5916de2f4e74b4db6
Yurai-Kitadate <yuurai18@gmail.com>
2023-04-13 09:35:55 +0900

    modpow変更

--- math/modpow.hpp
+++ math/modpow.hpp
@@ -1,12 +1,12 @@
 ll modpow(ll a, ll b, ll p)
 {
-    ll ans = 1;
+    ll res = 1;
     while (b)
     {
         if (b & 1)
-            (ans *= a) %= p;
-        (a *= a) %= p;
+            res = (res * a) % p;
+        a = (a * a) % p;
         b /= 2;
     }
-    return ans;
+    return res;
 }
