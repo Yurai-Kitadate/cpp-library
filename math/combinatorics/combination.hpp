7bf29c2d9d9408e92d93d48a06bae144de3abeb4
Yurai-Kitadate <yuurai18@gmail.com>
2023-04-13 16:07:56 +0900

    comb.c

--- math/combinatorics/combination.hpp
+++ math/combinatorics/combination.hpp
@@ -1,8 +1,32 @@
 struct Combination
 {
+    ll size;
     ll mod;
-    vector<ll> fac;
-    Combination()
+    ll f = 1;
+    vector<ll> fac = {f};
+    vector<ll> facinv;
+    Combination(ll size, ll p)
     {
+        mod = p;
+        rep(i, 1, size + 1)
+        {
+            f = f * i % mod;
+            fac.push_back(f);
+        }
+        f = modpow(f, mod - 2, mod);
+        facinv.push_back(f);
+        for (ll i = size; i > 0; i--)
+        {
+            // out(i);
+            f = f * i % mod;
+            facinv.push_back(f);
+        }
+        reverse(all(facinv));
     }
-}
+    ll c(ll n, ll r)
+    {
+        if (!(0 <= r && r <= n))
+            return 0;
+        return (((fac[n] * facinv[r]) % mod) * facinv[n - r]) % mod;
+    }
+};
