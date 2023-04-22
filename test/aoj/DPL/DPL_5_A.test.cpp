92c13fb000fb9e9f473325229dfdb65154faf41a
Yurai-Kitadate <yuurai18@gmail.com>
2023-04-22 11:05:01 +0900

    冪乗

---
+++ test/aoj/DPL/DPL_5_A.test.cpp
@@ -0,0 +1,10 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/7/DPL/5/DPL_5_E"
+#include "../../../template/template.hpp"
+#include "../../../math/modpow.hpp"
+#include "../../../math/combination.hpp"
+int main()
+{
+    LL(n, k);
+    ll mod = 1000000007;
+    out(modpow(k, n, mod));
+}
