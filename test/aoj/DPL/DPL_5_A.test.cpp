1c26cd051c76b94217d3734365591bf1c4b6ed48
Yurai-Kitadate <yuurai18@gmail.com>
2023-04-22 11:15:50 +0900

    url修正

--- test/aoj/DPL/DPL_5_A.test.cpp
+++ test/aoj/DPL/DPL_5_A.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/7/DPL/5/DPL_5_E"
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/7/DPL/5/DPL_5_A"
 #include "../../../template/template.hpp"
 #include "../../../math/modpow.hpp"
 #include "../../../math/combination.hpp"
